76fed22aba84cca76ef257c99d540a9c0cd3c0ce
sallaben <steven.allaben@gmail.com>
2022-05-12 20:48:48 -0400

    license, cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn parse(input: &str) -> String {
   let hcl: Value = hcl::from_str(input).unwrap();
+  // todo: extend HCL into Specific Language
   let ser: Result<String, Error> = serde_json::to_string(&hcl);
   match ser {
     Ok(o) => return o,
-    Err(e) => panic!("failed to serialize"), // todo: e
+    Err(_e) => panic!("failed to serialize"), // todo: handle error
   }
 }
