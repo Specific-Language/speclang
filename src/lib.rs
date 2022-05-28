317552829c0a28f0cfb8b3808cf0bd52db0576a1
Steve Allaben <steven.allaben@gmail.com>
2022-05-27 20:09:20 -0600

    wrap input with spec

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,9 @@ pub mod speclang {
   use wasm_bindgen::prelude::*;
 
   #[wasm_bindgen]
-  pub fn parse(input: &str) -> String {
-    let hcl: Value = hcl::from_str(input).unwrap();
+  pub fn parse(raw_input: &str) -> String {
+    let input = format!("input {{{}}}", raw_input);
+    let hcl: Value = hcl::from_str(&input).unwrap();
     let spec: Value = hydrate(&hcl);
     spec.to_string()
   }
@@ -20,8 +21,8 @@ pub mod speclang {
     if input.len() > 1 {
       for (key, value) in input {
         match key.as_str() {
-          "extends" => extends(&mut output, value),
-          _ => preserve(&mut output, key, value),
+          // "extends" => extends(&mut output, value),
+          _ => identity(&mut output, key, value),
         }
       }
     } else if input.len() == 1 {
@@ -33,19 +34,19 @@ pub mod speclang {
     output
   }
 
+  fn identity(output: &mut Value, key: &str, value: &Value) {
+    output[key] = hydrate(value);
+  }
+
   // fn lookup(input: &str) -> Value {
   //   // todo: actually lookup singles somewhere
   // }
 
-  fn extends(output: &mut Value, value: &Value) {
-    for (child_key, child_value) in value.as_object().unwrap() {
-      output[child_key] = hydrate(child_value);
-    }
-  }
-
-  fn preserve(output: &mut Value, key: &str, value: &Value) {
-    output[key] = hydrate(value);
-  }
+  // fn extends(output: &mut Value, value: &Value) {
+  //   for (child_key, child_value) in value.as_object().unwrap() {
+  //     output[child_key] = hydrate(child_value);
+  //   }
+  // }
 
   #[cfg(test)]
   mod test {
@@ -56,7 +57,9 @@ pub mod speclang {
       let input = "foo = \"bar\"";
       let output = parse(input);
       let expected_output = json!({
-        "foo": "bar"
+        "input": {
+          "foo": "bar"
+        }
       })
       .to_string();
       assert_eq!(output, expected_output);
@@ -67,7 +70,9 @@ pub mod speclang {
       let input = "foo {}";
       let output = parse(input);
       let expected_output = json!({
-        "foo": {}
+        "input": {
+          "foo": {}
+        }
       })
       .to_string();
       assert_eq!(output, expected_output);
