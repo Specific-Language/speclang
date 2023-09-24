e98bd0ecadab194da0ce65f94d0a4831e1f7b88a
sallaben <steven.allaben@gmail.com>
2023-09-24 11:19:28 -0600

    hcl bones

--- src/parser/context.rs
+++ src/parser/context.rs
@@ -1,10 +1,12 @@
-use crate::parser::expression::Expression;
+use crate::parser::expression::Computed;
+use crate::parser::expression::Literal;
+
 use serde_json::Map;
 use serde_json::Value;
 use std::collections::BTreeMap;
 
 pub struct Context {
-    tree: BTreeMap<String, Expression>,
+    tree: BTreeMap<String, Computed>,
 }
 
 impl Context {
@@ -15,57 +17,69 @@ impl Context {
     }
 
     pub fn from(map: Map<String, Value>) -> Self {
-        // flattens the map into a string->expression tree
-        // todo : objects and arrays
-        let mut tree = BTreeMap::new();
+        let mut context = Self::new();
         for (key, value) in map {
             match value {
                 Value::String(s) => {
-                    let expr = Expression::from(s.as_str()).unwrap();
-                    tree.insert(key, expr);
+                    let expr = Computed::from(s.as_str()).unwrap();
+                    context.set(&key, expr);
                 },
                 Value::Number(n) => {
-                    let expr = Expression::from(n.to_string().as_str()).unwrap();
-                    tree.insert(key, expr);
+                    let expr = Computed::from(n.to_string().as_str()).unwrap();
+                    context.set(&key, expr);
                 },
                 Value::Bool(b) => {
-                    let expr = Expression::from(b.to_string().as_str()).unwrap();
-                    tree.insert(key, expr);
+                    let expr = Computed::from(b.to_string().as_str()).unwrap();
+                    context.set(&key, expr);
                 },
+                Value::Object(o) => {
+                    let subcontext = Context::from(o);
+                    for (subkey, subvalue) in subcontext.tree {
+                        let name = format!("{}.{}", key, subkey);
+                        context.set(&name, subvalue);
+                    }
+                }
                 _ => panic!("Unexpected value type in context map")
             }
         }
-        Context { tree }
+        context
     }
 
-    pub fn get(&self, name: &str) -> Option<&Expression> {
+    pub fn get(&self, name: &str) -> Option<&Computed> {
         self.tree.get(name)
     }
 
-    pub fn set(&mut self, name: String, value: &str) -> Result<(), &'static str> {
-        let expr = Expression::from(value).unwrap();
-        self.tree.insert(name, expr);
+    pub fn set(&mut self, name: &str, expr: Computed) {
+        self.tree.insert(name.to_owned(), expr);
+    }
+
+    pub fn set_parsed(&mut self, name: &str, input: &str) -> Result<(), &'static str> {
+        let expr = Computed::from(input).unwrap();
+        self.set(name, expr);
         Ok(())
+    }
+
+    pub fn eval(&self, name: &str) -> Result<Literal, &'static str> {
+        self.get(name).ok_or("Variable not found in context")?.eval(self)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::parser::expression::Static;
 
     #[test]
     fn test_variables() {
-        let expression = Expression::from("${a + b}").unwrap();
+        let expression = Computed::from("${a + b}").unwrap();
         let mut context = Context::new();
-        context.set("a".to_owned(), "1.0").unwrap();
-        context.set("b".to_owned(), "2.0").unwrap();
+        context.set_parsed("a", "1.0").unwrap();
+        context.set_parsed("b", "2.0").unwrap();
         let result = expression.eval(&context).unwrap();
-        assert_eq!(result, Static::Number(3.0));
+        assert_eq!(result, Literal::Number(3.0));
 
-        context.set("b".to_owned(), "3.0").unwrap();
+        context.set_parsed("b", "3.0").unwrap();
         let result2 = expression.eval(&context).unwrap();
-        assert_eq!(result2, Static::Number(4.0));
+        assert_eq!(result2, Literal::Number(4.0));
     }
 
     #[test]
@@ -75,39 +89,51 @@ mod tests {
     }
 
     #[test]
+    fn test_context_set() {
+        let mut context = Context::new();
+        context.set_parsed("a", "1.0").unwrap();
+        context.set_parsed("b", "2.0").unwrap();
+        assert_eq!(context.tree.len(), 2);
+    }
+
+    #[test]
+    fn test_context_literal_eval() {
+        let mut context = Context::new();
+        context.set_parsed("a", "1.0").unwrap();
+        context.set_parsed("b", "2.0").unwrap();
+        let result = context.eval("a").unwrap();
+        assert_eq!(result, Literal::Number(1.0));
+    }
+
+    #[test]
+    fn test_context_computed_eval() {
+        let mut context = Context::new();
+        context.set_parsed("a", "${b+c}").unwrap();
+        context.set_parsed("b", "2.0").unwrap();
+        context.set_parsed("c", "${-z}").unwrap();
+        context.set_parsed("z", "5.0").unwrap();
+        let result = context.eval("a").unwrap();
+        assert_eq!(result, Literal::Number(-3.0));
+    }
+
+    #[test]
     fn test_context_from_map() {
         let mut map = Map::new();
         map.insert("a".to_owned(), serde_json::json!(1));
         map.insert("b".to_owned(), serde_json::json!(2));
         let context = Context::from(map);
         assert_eq!(context.tree.len(), 2);
+        let result = context.eval("a").unwrap();
+        assert_eq!(result, Literal::Number(1.0));
     }
 
     #[test]
-    fn test_context_set() {
-        let mut context = Context::new();
-        context.set("a".to_owned(), "1.0").unwrap();
-        context.set("b".to_owned(), "2.0").unwrap();
-        assert_eq!(context.tree.len(), 2);
-    }
-
-    #[test]
-    fn test_context_get() {
-        let mut context = Context::new();
-        context.set("a".to_owned(), "1.0").unwrap();
-        context.set("b".to_owned(), "2.0").unwrap();
-        let result = context.get("a").unwrap().eval(&context).unwrap();
-        assert_eq!(result, Static::Number(1.0));
-    }
-
-    #[test]
-    fn test_context_complex_get() {
-        let mut context = Context::new();
-        context.set("a".to_owned(), "${b+c}").unwrap();
-        context.set("b".to_owned(), "2.0").unwrap();
-        context.set("c".to_owned(), "${-z}").unwrap();
-        context.set("z".to_owned(), "5.0").unwrap();
-        let result = context.get("a").unwrap().eval(&context).unwrap();
-        assert_eq!(result, Static::Number(-3.0));
+    fn test_context_from_nested_object() {
+        let mut map = Map::new();
+        map.insert("a".to_owned(), serde_json::json!(1));
+        map.insert("foo".to_owned(), serde_json::json!({"bar": 2}));
+        let context = Context::from(map);
+        let result = context.eval("foo.bar").unwrap();
+        assert_eq!(result, Literal::Number(2.0));
     }
 }
--- src/parser/expression.rs
+++ src/parser/expression.rs
@@ -5,41 +5,41 @@ use pest::Parser;
 #[grammar = "parser/expression.pest"]
 pub struct ExpressionParser;
 
-#[derive(Debug, Clone)]
-pub enum TemplateItem {
-    Interpolated(Expression),
-    Literal(String),
-}
-
 #[derive(PartialEq, Debug, Clone)]
-pub enum Static {
+pub enum Literal {
     Number(f64),
     String(String),
     Bool(bool),
 }
 
-impl Static {
+impl Literal {
     pub fn to_string(&self) -> String {
         match self {
-            Static::String(s) => s.clone(),
-            Static::Bool(b) => b.to_string(),
-            Static::Number(n) => n.to_string(),
+            Literal::String(s) => s.clone(),
+            Literal::Bool(b) => b.to_string(),
+            Literal::Number(n) => n.to_string(),
         }
     }
 }
 
 #[derive(Debug, Clone)]
-pub enum Expression {
-    Unary(UnaryOp, Box<Expression>),
-    Binary(Box<Expression>, BinaryOp, Box<Expression>),
-    Literal(Static),
-    Reference(String),
-    Group(Box<Expression>),
-    Template(Vec<TemplateItem>),
-    Interpolated(Box<Expression>),
+pub enum TemplateItem {
+    Interpolated(Computed),
+    Literal(String),
 }
 
-impl Expression {
+#[derive(Debug, Clone)]
+pub enum Computed {
+    Unary(UnaryOp, Box<Computed>),
+    Binary(Box<Computed>, BinaryOp, Box<Computed>),
+    Literal(Literal),
+    Reference(String),
+    Group(Box<Computed>),
+    Template(Vec<TemplateItem>),
+    Interpolated(Box<Computed>),
+}
+
+impl Computed {
     pub fn from(input: &str) -> Result<Self, pest::error::Error<Rule>> {
         let mut parsed = ExpressionParser::parse(Rule::TEMPLATE, input)?;
         Ok(Self::from_pair(parsed.next().unwrap()))
@@ -49,21 +49,32 @@ impl Expression {
         match pair.as_rule() {
             Rule::TEMPLATE => {
                 let items = pair.into_inner().map(|p| TemplateItem::Interpolated(Self::from_pair(p))).collect();
-                Expression::Template(items)
+                Computed::Template(items)
             }
             Rule::INTERPOLATED => {
                 let inner = pair.into_inner().next().unwrap();
-                Expression::Interpolated(Box::new(Self::from_pair(inner)))
+                Computed::Interpolated(Box::new(Self::from_pair(inner)))
             }
-            Rule::NUMBER => Expression::Literal(Static::Number(pair.as_str().parse().unwrap())),
-            Rule::BOOL => Expression::Literal(Static::Bool(pair.as_str() == "true")),
-            Rule::STRING => Expression::Literal(Static::String(pair.as_str().to_string())),
+            Rule::GROUP => {
+                let inner = pair.into_inner().next().unwrap();
+                Computed::Group(Box::new(Self::from_pair(inner)))
+            }
+            Rule::NUMBER => Computed::Literal(Literal::Number(pair.as_str().parse().unwrap())),
+            Rule::BOOL => Computed::Literal(Literal::Bool(pair.as_str() == "true")),
+            Rule::STRING => Computed::Literal(Literal::String(pair.as_str().to_string())),
+            Rule::REFERENCE => {
+                match pair.as_str() {
+                    "true" => Computed::Literal(Literal::Bool(true)),
+                    "false" => Computed::Literal(Literal::Bool(false)),
+                    _ => Computed::Reference(pair.as_str().to_string()),
+                }
+            }
             Rule::LOGIC | Rule::COMPARE | Rule::MATH | Rule::MULTIPLY | Rule::EXPONENT => {
                 let mut inner = pair.into_inner();
                 let mut left = Self::from_pair(inner.next().unwrap());
                 while let Some(op) = inner.next() {
                     let right = Self::from_pair(inner.next().unwrap());
-                    left = Expression::Binary(
+                    left = Computed::Binary(
                         Box::new(left),
                         BinaryOp::from_str(op.as_str()).unwrap(),
                         Box::new(right),
@@ -72,56 +83,35 @@ impl Expression {
                 left
             }
             Rule::TERM => {
-                let inner = pair.into_inner();
-                let mut unary_op = None;
-                let mut expr = None;
-                for child in inner {
-                    if let Rule::UNARY_OP = child.as_rule() {
-                        unary_op = Some(child.as_str().to_string());
-                    } else {
-                        expr = Some(Self::from_pair(child));
-                    }
-                }            
-                match unary_op {
-                    Some(op) => Expression::Unary(
-                        UnaryOp::from_str(&op).unwrap(),
-                        Box::new(expr.unwrap())
-                    ),
-                    None => expr.unwrap(),
+                let mut inner = pair.into_inner();
+                let child = inner.next().expect("Expected at least one child");
+                if let Rule::UNARY_OP = child.as_rule() {
+                    let op = UnaryOp::from_str(child.as_str()).unwrap();
+                    let next_child = inner.next().expect("Expected expression after unary op");
+                    let expr = Self::from_pair(next_child);
+                    Computed::Unary(op, Box::new(expr))
+                } else {
+                    Self::from_pair(child)
                 }
-            }
-            Rule::REFERENCE => {
-                match pair.as_str() {
-                    "true" => Expression::Literal(Static::Bool(true)),
-                    "false" => Expression::Literal(Static::Bool(false)),
-                    _ => Expression::Reference(pair.as_str().to_string()),
-                }
-            }
-            Rule::GROUP => {
-                let inner = pair.into_inner().next().unwrap();
-                Expression::Group(Box::new(Self::from_pair(inner)))
             }
             _ => panic!("Unknown rule: {:?}", pair.as_rule()),
         }
     }
 
-    pub fn eval(&self, context: &crate::parser::Context) -> Result<Static, &'static str> {
+    pub fn eval(&self, context: &crate::parser::Context) -> Result<Literal, &'static str> {
         match self {
-            Expression::Unary(op, expr) => {
+            Computed::Literal(val) => Ok(val.clone()),
+            Computed::Reference(name) => context.eval(name),
+            Computed::Unary(op, expr) => {
                 let val = expr.eval(context)?;
                 Self::eval_unary_operation(op, val)
             }
-            Expression::Binary(left, op, right) => {
+            Computed::Binary(left, op, right) => {
                 let left_val = left.eval(context)?;
                 let right_val = right.eval(context)?;
                 Self::eval_binary_operation(op, left_val, right_val)
             }
-            Expression::Literal(val) => Ok(val.clone()),
-            Expression::Reference(name) => {
-                let expr = context.get(name).ok_or("Variable not found in context")?;
-                expr.eval(context)
-            }
-            Expression::Template(items) => {
+            Computed::Template(items) => {
                 if items.len() == 1 {
                     if let TemplateItem::Interpolated(expr) = &items[0] {
                         return expr.eval(context);
@@ -137,20 +127,20 @@ impl Expression {
                         }
                     }
                 }
-                Ok(Static::String(result))
+                Ok(Literal::String(result))
             },
             _ => unreachable!(),
         }
     }
 
-    fn eval_unary_operation(op: &UnaryOp, val: Static) -> Result<Static, &'static str> {
+    fn eval_unary_operation(op: &UnaryOp, val: Literal) -> Result<Literal, &'static str> {
         match op {
             UnaryOp::LogicNot => match val {
-                Static::Bool(b) => Ok(Static::Bool(!b)),
+                Literal::Bool(b) => Ok(Literal::Bool(!b)),
                 _ => Err("Invalid type for NOT operation"),
             },
             UnaryOp::MathNegative => match val {
-                Static::Number(n) => Ok(Static::Number(-n)),
+                Literal::Number(n) => Ok(Literal::Number(-n)),
                 _ => Err("Invalid type for negative operation"),
             },
         }
@@ -158,9 +148,9 @@ impl Expression {
 
     fn eval_binary_operation(
         op: &BinaryOp,
-        left_val: Static,
-        right_val: Static,
-    ) -> Result<Static, &'static str> {
+        left_val: Literal,
+        right_val: Literal,
+    ) -> Result<Literal, &'static str> {
         match op {
             | BinaryOp::MathAdd
             | BinaryOp::MathSubtract
@@ -168,41 +158,41 @@ impl Expression {
             | BinaryOp::MathDivide
             | BinaryOp::MathModulus
             | BinaryOp::MathPower => match (left_val, right_val) {
-                (Static::Number(l), Static::Number(r)) => match op {
-                    BinaryOp::MathAdd => Ok(Static::Number(l + r)),
-                    BinaryOp::MathSubtract => Ok(Static::Number(l - r)),
-                    BinaryOp::MathMultiply => Ok(Static::Number(l * r)),
+                (Literal::Number(l), Literal::Number(r)) => match op {
+                    BinaryOp::MathAdd => Ok(Literal::Number(l + r)),
+                    BinaryOp::MathSubtract => Ok(Literal::Number(l - r)),
+                    BinaryOp::MathMultiply => Ok(Literal::Number(l * r)),
                     BinaryOp::MathDivide => {
                         if r == 0.0 {
                             Err("Division by zero")
                         } else {
-                            Ok(Static::Number(l / r))
+                            Ok(Literal::Number(l / r))
                         }
                     }
-                    BinaryOp::MathModulus => Ok(Static::Number(l % r)),
-                    BinaryOp::MathPower => Ok(Static::Number(l.powf(r))),
+                    BinaryOp::MathModulus => Ok(Literal::Number(l % r)),
+                    BinaryOp::MathPower => Ok(Literal::Number(l.powf(r))),
                     _ => unreachable!(),
                 },
                 _ => Err("Invalid operands for mathematical operation"),
             },
             BinaryOp::LogicAnd | BinaryOp::LogicOr => match (left_val, right_val) {
-                (Static::Bool(l), Static::Bool(r)) => match op {
-                    BinaryOp::LogicAnd => Ok(Static::Bool(l && r)),
-                    BinaryOp::LogicOr => Ok(Static::Bool(l || r)),
+                (Literal::Bool(l), Literal::Bool(r)) => match op {
+                    BinaryOp::LogicAnd => Ok(Literal::Bool(l && r)),
+                    BinaryOp::LogicOr => Ok(Literal::Bool(l || r)),
                     _ => unreachable!(),
                 },
                 _ => Err("Invalid operands for logical operation"),
             },
             BinaryOp::LogicComparison(comp_op) => match (left_val, right_val) {
-                (Static::Number(l), Static::Number(r)) => match comp_op {
-                    ComparisonOp::LessThan => Ok(Static::Bool(l < r)),
-                    ComparisonOp::GreaterThan => Ok(Static::Bool(l > r)),
-                    ComparisonOp::LessThanOrEqual => Ok(Static::Bool(l <= r)),
-                    ComparisonOp::GreaterThanOrEqual => Ok(Static::Bool(l >= r)),
-                    ComparisonOp::Equals => Ok(Static::Bool((l - r).abs() < f64::EPSILON)),
+                (Literal::Number(l), Literal::Number(r)) => match comp_op {
+                    ComparisonOp::LessThan => Ok(Literal::Bool(l < r)),
+                    ComparisonOp::GreaterThan => Ok(Literal::Bool(l > r)),
+                    ComparisonOp::LessThanOrEqual => Ok(Literal::Bool(l <= r)),
+                    ComparisonOp::GreaterThanOrEqual => Ok(Literal::Bool(l >= r)),
+                    ComparisonOp::Equals => Ok(Literal::Bool((l - r).abs() < f64::EPSILON)),
                 },
-                (Static::Bool(l), Static::Bool(r)) => match comp_op {
-                    ComparisonOp::Equals => Ok(Static::Bool(l == r)),
+                (Literal::Bool(l), Literal::Bool(r)) => match comp_op {
+                    ComparisonOp::Equals => Ok(Literal::Bool(l == r)),
                     _ => Err("Invalid comparison for booleans"),
                 },
                 _ => Err("Invalid operands for comparison operation"),
@@ -223,131 +213,131 @@ mod tests {
 
     #[test]
     fn test_bool() {
-        let expression = Expression::from("true").unwrap();
+        let expression = Computed::from("true").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(true));
+        assert_eq!(result, Literal::Bool(true));
     }
 
     #[test]
     fn test_wrapped() {
-        let expression = Expression::from("${true}").unwrap();
+        let expression = Computed::from("${true}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(true));
+        assert_eq!(result, Literal::Bool(true));
     }
 
     #[test]
     fn test_addition() {
-        let expression = Expression::from("${1 + 2}").unwrap();
+        let expression = Computed::from("${1 + 2}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(3.0));
+        assert_eq!(result, Literal::Number(3.0));
     }
 
     #[test]
     fn test_addition_complex() {
-        let expression = Expression::from("${1 + 2 + 3}").unwrap();
+        let expression = Computed::from("${1 + 2 + 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(6.0));
+        assert_eq!(result, Literal::Number(6.0));
     }
 
     #[test]
     fn test_subtraction() {
-        let expression = Expression::from("${1 - 2 - 3}").unwrap();
+        let expression = Computed::from("${1 - 2 - 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(-4.0));
+        assert_eq!(result, Literal::Number(-4.0));
     }
 
     #[test]
     fn test_multiplication() {
-        let expression = Expression::from("${1 * 2 * 3}").unwrap();
+        let expression = Computed::from("${1 * 2 * 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(6.0));
+        assert_eq!(result, Literal::Number(6.0));
     }
 
     #[test]
     fn test_division() {
-        let expression = Expression::from("${1 / 2}").unwrap();
+        let expression = Computed::from("${1 / 2}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(0.5));
+        assert_eq!(result, Literal::Number(0.5));
     }
 
     #[test]
     fn test_modulus() {
-        let expression = Expression::from("${5 % 2 % 3}").unwrap();
+        let expression = Computed::from("${5 % 2 % 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(1.0));
+        assert_eq!(result, Literal::Number(1.0));
     }
 
     #[test]
     fn test_power() {
-        let expression = Expression::from("${1 ^ 2 ^ 3}").unwrap();
+        let expression = Computed::from("${1 ^ 2 ^ 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(1.0));
+        assert_eq!(result, Literal::Number(1.0));
     }
 
     #[test]
     fn test_and() {
-        let expression = Expression::from("${true && false && true}").unwrap();
+        let expression = Computed::from("${true && false && true}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(false));
+        assert_eq!(result, Literal::Bool(false));
     }
 
     #[test]
     fn test_or() {
-        let expression = Expression::from("${true || false || true}").unwrap();
+        let expression = Computed::from("${true || false || true}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(true));
+        assert_eq!(result, Literal::Bool(true));
     }
 
     #[test]
     fn test_less_than() {
-        let expression = Expression::from("${1 < 2}").unwrap();
+        let expression = Computed::from("${1 < 2}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(true));
+        assert_eq!(result, Literal::Bool(true));
     }
 
     #[test]
     fn test_greater_than() {
-        let expression = Expression::from("${2 > 3}").unwrap();
+        let expression = Computed::from("${2 > 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(false));
+        assert_eq!(result, Literal::Bool(false));
     }
 
     #[test]
     fn test_less_than_or_equal() {
-        let expression = Expression::from("${3 <= 3}").unwrap();
+        let expression = Computed::from("${3 <= 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(true));
+        assert_eq!(result, Literal::Bool(true));
     }
 
     #[test]
     fn test_greater_than_or_equal() {
-        let expression = Expression::from("${2 >= 3}").unwrap();
+        let expression = Computed::from("${2 >= 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(false));
+        assert_eq!(result, Literal::Bool(false));
     }
 
     #[test]
     fn test_equals() {
-        let expression = Expression::from("${1 == 2}").unwrap();
+        let expression = Computed::from("${1 == 2}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(false));
+        assert_eq!(result, Literal::Bool(false));
 
-        let expression2 = Expression::from("${2 == 2}").unwrap();
+        let expression2 = Computed::from("${2 == 2}").unwrap();
         let result2 = expression2.eval(&CONTEXT).unwrap();
-        assert_eq!(result2, Static::Bool(true));
+        assert_eq!(result2, Literal::Bool(true));
     }
 
     #[test]
     fn test_not() {
-        let expression = Expression::from("${!true}").unwrap();
+        let expression = Computed::from("${!true}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Bool(false));
+        assert_eq!(result, Literal::Bool(false));
     }
 
     #[test]
     fn test_parentheses() {
-        let expression = Expression::from("${(1 + 2) * 3}").unwrap();
+        let expression = Computed::from("${(1 + 2) * 3}").unwrap();
         let result = expression.eval(&CONTEXT).unwrap();
-        assert_eq!(result, Static::Number(9.0));
+        assert_eq!(result, Literal::Number(9.0));
     }
 }
